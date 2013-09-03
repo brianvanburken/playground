4d134b1fa16ea89efde2dea66d14d3a35d711195
Brian van Burken <brianburkertje@hotmail.com>
2013-09-03 12:21:51 +0200

    Added styles for changing color and added comments.

--- modules/nprogress.rb
+++ modules/nprogress.rb
@@ -10,10 +10,22 @@ if yes?("Use nprogress for Turbolinks loading indicator?", :yellow)
     "\n#= require nprogress\n\n"
   end
 
+  # Links NProgress with Turbolinks
   append_file "app/assets/javascripts/application.js.coffee" do
+    "\n" +
+    "# Link Turbolink status changes with NProgress \n" +
     "$(document).on 'page:fetch',   -> NProgress.start()\n" +
     "$(document).on 'page:change',  -> NProgress.done()\n" +
     "$(document).on 'page:restore', -> NProgress.remove()\n"
+  end
+
+  # Adds the selectors for changing NProgress colours.
+  append_file "app/assets/stylesheets/application.css.sass" do
+    "\n" +
+    "// Styles for NProgress plugin \n" +
+    "#nprogress .bar { background: #29d; }\n" +
+    "#nprogress .peg { box-shadow: 0 0 10px #29d, 0 0 5px #29d; }\n" +
+    "#nprogress .spinner-icon { border-top-color: #29d; border-left-color: #29d; }\n"
   end
 
 end
