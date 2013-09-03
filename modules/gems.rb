d5bcade545458759b887cc0d79554789343709a9
Brian van Burken <info@brianvanburken.nl>
2013-09-03 18:46:28 +0200

    Added some usefull gems for development.

--- modules/gems.rb
+++ modules/gems.rb
@@ -1,25 +1,26 @@
-# Useful SASS mixins (http://bourbon.io/)
-gem 'bourbon'
-
 # Checks if we're still following the best practices and picks out bad smells
 gem 'rails_best_practices'
 
 # Simple pagination gem (https://github.com/amatsuda/kaminari)
 gem 'kaminari', '~> 0.14.1'
 
+# Useful SASS mixins (http://bourbon.io/)
+gem 'bourbon'
+
 # Simple form builder (https://github.com/plataformatec/simple_form)
 #gem "simple_form", '~> 2.1.0'
 
 gem_group :development do
-  gem 'binding_of_caller', '~> 0.7.2' # Needed for better_errors
-  gem 'better_errors', '~> 0.9.0'
-
-  # Rack Mini-Profiler for catching performance bottlenecks early stage
-  gem 'rack-mini-profiler', '~> 0.1.29'
-
+  gem 'binding_of_caller' # Needed for better_errors
+  gem 'better_errors'
   # Opens email in browser instead of sending it
-  gem 'letter_opener', '~> 1.1.2'
-
+  gem 'letter_opener'
   # Silence those annoying assests pipeline loglines
-  gem 'quiet_assets', '~> 1.0.2'
+  gem 'quiet_assets'
+  # Middleware that displays speed badge for every html page.
+  gem 'rack-mini-profiler'
+  # Gem for detecting N+1 and other performance improvements
+  gem 'bullet'
+  # Brakeman is a static analysis tool which checks Ruby on Rails applications for security vulnerabilities.
+  gem 'brakeman'
 end
