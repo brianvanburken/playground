46554d337303109efc96e556123f3ce5a65424c0
Brian van Burken <brian@creativeview.nl>
2015-07-26 19:56:10 +0200

    Added codeclimate test reporter to test_helper.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
-ENV['RAILS_ENV'] ||= 'test'
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.startENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
