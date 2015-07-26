a2052a1fee5e9678ce06e67d027bbf99e1342f33
Brian van Burken <brian@creativeview.nl>
2015-07-26 10:27:57 +0200

    Added validation on literature model.

--- app/models/literature.rb
+++ app/models/literature.rb
@@ -12,4 +12,13 @@
 #
 
 class Literature < ActiveRecord::Base
+  validates :title, :short_description, :content, :published_at, presence: true
+  validate :published_in_the_past
+
+  private
+    def published_in_the_past
+      if published_at && published_at > Date.today
+        errors.add(:published_at, "can't be in the future")
+      end
+    end
 end
--- test/models/literature_test.rb
+++ test/models/literature_test.rb
@@ -14,7 +14,23 @@
 require 'test_helper'
 
 class LiteratureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should validate_presence_of(:title)
+  should validate_presence_of(:short_description)
+  should validate_presence_of(:content)
+  should validate_presence_of(:published_at)
+
+  test "published_at does not accept dates in the future" do
+    literature = literatures(:one)
+    literature.published_at = 1.day.from_now
+    assert_not literature.valid?
+    assert literature.errors.include?(:published_at)
+    assert_equal ["can't be in the future"], literature.errors.get(:published_at)
+  end
+
+  test "published_at only accepts dates in the past" do
+    literature = literatures(:one)
+    literature.published_at = 1.day.ago
+    assert literature.valid?
+    assert_not literature.errors.include?(:published_at)
+  end
 end
