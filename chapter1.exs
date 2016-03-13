7acfc5eadbf16c7696efad09568c34834084bd4f
Brian van Burken <brianvanburken@outlook.com>
2016-03-13 18:47:39 +0100

    Define unless macro.

---
+++ chapter1.exs
@@ -0,0 +1,23 @@
+defmodule My do
+  defmacro unless(condition, clauses) do
+    do_clause = Keyword.get(clauses, :do, nil)
+    else_clause = Keyword.get(clauses, :else, nil)
+    quote do
+      case unquote(condition) do
+        val when val in [false, nil] -> unquote(do_clause)
+        _                            -> unquote(else_clause)
+      end
+    end
+  end
+end
+
+defmodule Test do
+  import Kernel, except: [ unless: 2 ]
+  import My
+
+  unless 1 == 2 do
+    IO.puts "1 != 2"
+  else
+    IO.puts "1 == 2"
+  end
+end
