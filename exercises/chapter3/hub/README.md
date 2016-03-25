52469f178fd0e379c0cdcc9736a98d57ac7eb39a
Brian van Burken <brianvanburken@outlook.com>
2016-03-25 12:28:39 +0100

    Add some documentation explaining the working of the generation.

--- exercises/chapter3/hub/README.md
+++ exercises/chapter3/hub/README.md
@@ -1,5 +1,9 @@
 # Hub
 
+This project showcases the compilation of functions against an external API.
+The API for this project is the Pokemon API.
+Documentation can be found here: http://pokeapi.co/docsv2/#pokemon
+
 ## Installation
 
 ```
--- exercises/chapter3/hub/lib/hub.ex
+++ exercises/chapter3/hub/lib/hub.ex
@@ -2,6 +2,14 @@ defmodule Hub do
 
   HTTPotion.start
 
+  @doc """
+  Fetch all the first 151 pokemons of the first generation and define methods
+  for each based on the name. When called it uses the URL from the API result
+  to fetch additional information on runtime. The fetching of additional
+  information could be done on compile time. But to speed up the generation of
+  the functions I decided to fetch it on runtime. It would take a long time to
+  fetch 151 Pokemons using individual requests.
+  """
   "http://pokeapi.co/api/v2/pokemon/?limit=151&offset=0"
   |> HTTPotion.get([ "User-Agent": "Elixir" ])
   |> Map.get(:body)
