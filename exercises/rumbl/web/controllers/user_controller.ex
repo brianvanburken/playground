2a775a4ce818f5464618223c249876f3e75afda7
Brian van Burken <brianvanburken@outlook.com>
2016-05-07 19:31:38 +0200

    Finished Chapter 6.

--- exercises/rumbl/web/controllers/user_controller.ex
+++ exercises/rumbl/web/controllers/user_controller.ex
@@ -2,7 +2,7 @@ defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
   alias Rumbl.User
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def new(conn, _params) do
     changeset = User.changeset(%User{})
@@ -30,17 +30,6 @@ defmodule Rumbl.UserController do
       {:error, changeset} ->
         conn
         |> render("new.html", changeset: changeset)
-    end
-  end
-
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: page_path(conn, :index))
-      |> halt
     end
   end
 end
