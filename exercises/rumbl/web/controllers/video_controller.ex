9e7a1fc82b332f2b46a7a7302e6bffe8ef6f902e
Brian van Burken <brianvanburken@outlook.com>
2016-05-08 14:43:41 +0200

    Finished Chapter 7.

--- exercises/rumbl/web/controllers/video_controller.ex
+++ exercises/rumbl/web/controllers/video_controller.ex
@@ -2,7 +2,9 @@ defmodule Rumbl.VideoController do
   use Rumbl.Web, :controller
 
   alias Rumbl.Video
+  alias Rumbl.Category
 
+  plug :load_categories when action in [:new, :create, :edit, :update]
   plug :scrub_params, "video" when action in [:create, :update]
 
   def action(conn, _) do
@@ -70,4 +72,13 @@ defmodule Rumbl.VideoController do
   end
 
   defp user_videos(user), do: assoc(user, :videos)
+
+  defp load_categories(conn, _) do
+    query =
+      Category
+      |> Category.alphabetical
+      |> Category.names_and_ids
+    categories = Repo.all query
+    assign(conn, :categories, categories)
+  end
 end
