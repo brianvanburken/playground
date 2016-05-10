712eb1464269fd75e86376a2cf8951b51a1a1e4b
Brian van Burken <brianvanburken@outlook.com>
2016-05-10 10:05:34 +0200

    Fix confusing step in book.

--- exercises/rumbl/web/router.ex
+++ exercises/rumbl/web/router.ex
@@ -20,7 +20,6 @@ defmodule Rumbl.Router do
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
     resources "/sessions", SessionController, only: [:new, :create, :delete]
-    resources "/videos", VideoController
   end
 
   scope "/manage", Rumbl do
