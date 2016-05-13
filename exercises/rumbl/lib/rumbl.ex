b9f1c2b868e7e636282b935bfa7c339bc10394b5
Brian van Burken <brianvanburken@outlook.com>
2016-05-13 22:50:02 +0200

    Finished Chapter 11.

--- exercises/rumbl/lib/rumbl.ex
+++ exercises/rumbl/lib/rumbl.ex
@@ -7,12 +7,9 @@ defmodule Rumbl do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
-      # Start the Ecto repository
+      supervisor(Rumbl.InfoSys.Supervisor, []), # new supervisor
       supervisor(Rumbl.Repo, []),
-      # Here you could define other workers and supervisors as children
-      # worker(Rumbl.Worker, [arg1, arg2, arg3]),
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
