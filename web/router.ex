d0c60a1b8e262dd997db67bf88ad0728ffb56d97
Brian van Burken <brianvanburken@outlook.com>
2016-05-19 09:58:38 +0200

    Add poll model.

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule PhoenixStrawpoll.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/polls", PollController
   end
 
   # Other scopes may use custom stacks.
