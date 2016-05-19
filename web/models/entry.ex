25e7c9a30e5ab66c01d44e519ae37f6f1ec49104
Brian van Burken <brianvanburken@outlook.com>
2016-05-19 09:59:45 +0200

    Create entry model.

---
+++ web/models/entry.ex
@@ -0,0 +1,25 @@
+defmodule PhoenixStrawpoll.Entry do
+  use PhoenixStrawpoll.Web, :model
+
+  schema "entries" do
+    field :title, :string
+    field :votes, :integer
+    belongs_to :poll, PhoenixStrawpoll.Poll
+
+    timestamps
+  end
+
+  @required_fields ~w(title votes)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
