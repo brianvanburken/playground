4aabf219ab8a57b6f3ae3f1d9598d7e2355a6c0d
Brian van Burken <brianvanburken@outlook.com>
2016-05-24 10:07:31 +0200

    Add webpack.

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+
+function join(dest) { return path.resolve(__dirname, dest); }
+
+function web(dest) { return join('web/static/' + dest); }
+
+var config = module.exports = {
+  entry: {
+    application: [
+      web('css/application.sass'),
+      web('js/application.js'),
+    ],
+  },
+
+  output: {
+    path: join('priv/static'),
+    filename: 'js/application.js',
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.sass'],
+    modulesDirectories: ['node_modules'],
+  },
+
+  module: {
+    noParse: /vendor\/phoenix/,
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,el
+        loader: 'babel',
+        query: {
+          cacheDirectory: true,
+          plugins: ['transform-decorators-legacy'],
+          presets: ['react', 'es2015', 'stage-2', 'stage-0'],
+        },
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass?indentedSyntax&includePaths[]=' + __dirname +  '/node_modules'),
+      },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('css/application.css'),
+  ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true })
+  );
+}
