1e33d1d896cc0a041d7bc45347d8980ab7e6cece
Brian van Burken <brianvanburken@outlook.com>
2016-05-24 12:18:24 +0200

    Add React & Redux start.

--- web/router.ex
+++ web/router.ex
@@ -9,18 +9,9 @@ defmodule PhoenixTrello.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", PhoenixTrello do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", PhoenixTrello do
-  #   pipe_through :api
-  # end
 end
