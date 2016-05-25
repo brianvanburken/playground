5e6bac01f90fe8e8d0a246b1bc09804ae235bf36
Brian van Burken <brianvanburken@outlook.com>
2016-05-25 09:56:05 +0200

    Add session controller, routes and views.

---
+++ web/controllers/api/v1/session_controller.ex
@@ -0,0 +1,22 @@
+defmodule PhoenixTrello.SessionController do
+  use PhoenixTrello.Web, :controller
+
+  plug :scrub_params, "session" when action in [:create]
+
+  def create(conn, %{"session" => session_params}) do
+    case PhoenixTrello.Session.authenticate(session_params) do
+      {:ok, user} ->
+        {:ok, jwt, _full_claims} = user |> Guardian.encode_and_sign(:token)
+
+        conn
+        |> put_status(:created)
+        |> render("show.json", jwt: jwt, user: user)
+
+      :error ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render("error.json")
+    end
+  end
+
+end
--- web/router.ex
+++ web/router.ex
@@ -11,6 +11,8 @@ defmodule PhoenixTrello.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/api", PhoenixTrello do
@@ -18,6 +20,8 @@ defmodule PhoenixTrello.Router do
 
     scope "/v1" do
       post "/registrations", RegistrationController, :create
+      post "/sessions", SessionController, :create
+      delete "/sessions", SessionController, :delete
     end
   end
 
