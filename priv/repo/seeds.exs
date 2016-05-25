6437eb526dc55a09ceb78093cfb8801ce83c9f9e
Brian van Burken <brianvanburken@outlook.com>
2016-05-25 09:30:38 +0200

    Add users in database seed.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,12 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     PhoenixTrello.Repo.insert!(%SomeModel{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias PhoenixTrello.{Repo, User}
+
+[
+  %{
+    first_name: "John",
+    last_name: "Doe",
+    email: "john@phoenix-trello.com",
+    password: "12345678"
+  }
+]
+|> Enum.map(&User.changeset(%User{}, &1))
+|> Enum.each(&Repo.insert!(&1))
