8fcd01dcc4a931ca2f473e4c3a6853f70999b0b2
Brian van Burken <brianvanburken@outlook.com>
2016-07-26 11:16:22 +0200

    Exercise 6.1.5 Replace ServerProcess with GenServer.

--- chap6/todo_list.ex
+++ chap6/todo_list.ex
@@ -1,19 +1,17 @@
 defmodule TodoServer do
-  def start do
-    ServerProcess.start(TodoServer)
-  end
+  use GenServer
+
+  def start, do: GenServer.start(TodoServer, nil)
 
   def add_entry(todo_server, new_entry) do
-    ServerProcess.cast(todo_server, {:add_entry, new_entry})
+    GenServer.cast(todo_server, {:add_entry, new_entry})
   end
 
   def entries(todo_server, date) do
-    ServerProcess.call(todo_server, {:entries, date})
+    GenServer.call(todo_server, {:entries, date})
   end
 
-  def init do
-    TodoList.new
-  end
+  def init(_), do: {:ok, TodoList.new}
 
   def handle_cast({:add_entry, new_entry}, todo_list) do
     TodoList.add_entry(todo_list, new_entry)
@@ -23,53 +21,6 @@ defmodule TodoServer do
     {TodoList.entries(todo_list, date), todo_list}
   end
 end
-
-
-
-defmodule ServerProcess do
-  def start(callback_module) do
-    spawn(fn ->
-      initial_state = callback_module.init
-      loop(callback_module, initial_state)
-    end)
-  end
-
-  defp loop(callback_module, current_state) do
-    receive do
-      {:call, request, caller} ->
-        {response, new_state} = callback_module.handle_call(
-          request,
-          current_state
-        )
-
-        send(caller, {:response, response})
-        loop(callback_module, new_state)
-
-      {:cast, request} ->
-        new_state = callback_module.handle_cast(
-          request,
-          current_state
-        )
-
-        loop(callback_module, new_state)
-    end
-  end
-
-  def call(server_pid, request) do
-    send(server_pid, {:call, request, self})
-
-    receive do
-      {:response, response} ->
-        response
-    end
-  end
-
-  def cast(server_pid, request) do
-    send(server_pid, {:cast, request})
-  end
-end
-
-
 
 defmodule TodoList do
   defstruct auto_id: 1, entries: HashDict.new
