9ec532960c9fcd9019748aa7d0bbc0357f38ee85
Brian van Burken <brianvanburken@outlook.com>
2016-08-19 14:55:40 +0200

    Chapter 8.3.3

--- chap8/todo/lib/todo/cache.ex
+++ chap8/todo/lib/todo/cache.ex
@@ -1,12 +1,13 @@
 defmodule Todo.Cache do
   use GenServer
 
-  def start do
-    GenServer.start(__MODULE__, nil)
+  def start_link do
+    IO.puts "Starting to-do cache."
+    GenServer.start_link(__MODULE__, nil, name: :todo_cache)
   end
 
-  def server_process(cache_pid, todo_list_name) do
-    GenServer.call(cache_pid, {:server_process, todo_list_name})
+  def server_process(todo_list_name) do
+    GenServer.call(:todo_cache, {:server_process, todo_list_name})
   end
 
   def init(_) do
