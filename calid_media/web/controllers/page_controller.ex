b6d5126d466d11951a3ddc60d4320d14b8c12a86
Brian van Burken <brianvanburken@outlook.com>
2016-10-25 22:18:34 +0200

    Add show view for files

--- calid_media/web/controllers/page_controller.ex
+++ calid_media/web/controllers/page_controller.ex
@@ -2,10 +2,20 @@ defmodule CalidMedia.PageController do
   use CalidMedia.Web, :controller
 
   def index(conn, _params) do
+    render conn, "index.html", files: media_files
+  end
+
+  def show(conn, %{"filename" => filename}) do
+    render conn, "show.html", filename: filename
+  end
+
+  defp media_files do
     media_dir = "./priv/static/media"
     filetype = [".mp4", ".mp3"]
     {:ok, files} = File.ls(media_dir)
-    filtered_files = Enum.filter(files, fn(file) -> String.ends_with?(file, filetype) end)
-    render conn, "index.html", files: filtered_files
+    files
+    |> Stream.filter(fn(file) -> String.ends_with?(file, filetype) end)
+    |> Stream.map(fn(file) -> %{filename: file} end)
+    |> Enum.to_list
   end
 end
