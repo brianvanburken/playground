d12919fb902efaa5895e3963d664b70138e9821c
Brian van Burken <brianvanburken@outlook.com>
2016-10-25 21:51:08 +0200

    Show filtered files on index

--- calid_media/web/controllers/page_controller.ex
+++ calid_media/web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule CalidMedia.PageController do
   use CalidMedia.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    media_dir = "./priv/static/media"
+    filetype = [".mp4", ".mp3"]
+    {:ok, files} = File.ls(media_dir)
+    filtered_files = Enum.filter(files, fn(file) -> String.ends_with?(file, filetype) end)
+    render conn, "index.html", files: filtered_files
   end
 end
