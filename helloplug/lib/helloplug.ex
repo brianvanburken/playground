124e3cd7d782c1af46177b907db4d1da0e99b280
Brian van Burken <brianvanburken@outlook.com>
2016-10-26 18:33:07 +0200

    First endpoint

--- helloplug/lib/helloplug.ex
+++ helloplug/lib/helloplug.ex
@@ -1,2 +1,13 @@
 defmodule Helloplug do
+  def init(default_opts) do
+    IO.puts "starting up Helloplug..."
+    default_opts
+  end
+
+  def call(conn, _opts) do
+    IO.puts "saying Hello!"
+    conn
+    |> Plug.Conn.put_resp_header("Server", "Plug")
+    |> Plug.Conn.send_resp(conn, 200, "Hello, world!")
+  end
 end
