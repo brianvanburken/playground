9f4571a8f6d34fd494412a8899e996920a188ec5
Brian van Burken <brianvanburken@outlook.com>
2016-10-26 16:47:57 +0200

    Display video/audio HTML tag based on filetype

--- calid_media/web/views/page_view.ex
+++ calid_media/web/views/page_view.ex
@@ -1,3 +1,13 @@
 defmodule CalidMedia.PageView do
   use CalidMedia.Web, :view
+
+  def filetype(filename) do
+    filename
+    |> String.split(".")
+    |> Enum.at(1)
+    |> get_file_type
+  end
+
+  defp get_file_type("mp3"), do: "mpeg"
+  defp get_file_type("mp4"), do: "mp4"
 end
