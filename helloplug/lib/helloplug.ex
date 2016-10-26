50425961f30203d9aedc0b1d2098f5eddb39ee87
Brian van Burken <brianvanburken@outlook.com>
2016-10-26 18:40:35 +0200

    Add a few example routes

--- helloplug/lib/helloplug.ex
+++ helloplug/lib/helloplug.ex
@@ -6,8 +6,17 @@ defmodule Helloplug do
 
   def call(conn, _opts) do
     IO.puts "saying Hello!"
-    conn
-    |> Plug.Conn.put_resp_header("Server", "Plug")
-    |> Plug.Conn.send_resp(conn, 200, "Hello, world!")
+    conn |> route
   end
+
+  def route(%{method: "GET", path_info: ["hello"]} = conn) do
+    conn |> Plug.Conn.send_resp(200, "Hello, world!")
+  end
+
+  def route(%{method: "GET", path_info: ["users", user_id]} = conn) do
+    conn |> Plug.Conn.send_resp(200, "You requested user #{user_id}")
+  end
+
+  def route(conn),
+    do: conn |> Plug.Conn.send_resp(404, "Couldn't find that page, sorry!")
 end
