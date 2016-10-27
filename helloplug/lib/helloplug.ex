a73da205758fd94d0a04e2bf018c20b88e4f64f8
Brian van Burken <brianvanburken@outlook.com>
2016-10-27 09:13:57 +0200

    Add templates

--- helloplug/lib/helloplug.ex
+++ helloplug/lib/helloplug.ex
@@ -16,7 +16,10 @@ defmodule Helloplug do
   end
 
   def route("GET", ["users", user_id], conn) do
-    conn |> Plug.Conn.send_resp(200, "You requested user #{user_id}")
+    page_contents = EEx.eval_file("templates/show_user.eex", [ user_id: user_id ])
+    conn
+    |> Plug.Conn.put_resp_content_type("text/html")
+    |> Plug.Conn.send_resp(200, page_contents)
   end
 
   def route(_method, _path, conn),
