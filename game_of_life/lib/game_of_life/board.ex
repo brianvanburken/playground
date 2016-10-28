10bcabf1d056aa107b9fc11b2a9d1f176c9bd012
Brian van Burken <brianvanburken@outlook.com>
2016-10-28 16:03:25 +0200

    Add board logic

---
+++ game_of_life/lib/game_of_life/board.ex
@@ -0,0 +1,41 @@
+defmodule GameOfLife.Board do
+  def add_cells(alive_cells, new_cells) do
+    alive_cells ++ new_cells
+    |> Enum.uniq
+  end
+
+  def remove_cells(alive_cells, kill_cells) do
+    alive_cells -- kill_cells
+  end
+
+  def keep_alive_tick(alive_cells) do
+    alive_cells
+    |> Enum.map(&(Task.Supervisor.async(
+      {GameOfLife.TaskSupervisor, GameOfLife.NodeManager.random_node},
+      GameOfLife.Board, :keep_alive_or_nilify, [alive_cells, &1])))
+    |> Enum.map(&Task.await/1)
+    |> remove_nil_cells
+  end
+
+  def keep_alive_or_nilify(alive_cells, cell) do
+    if GameOfLife.Cell.keep_alive?(alive_cells, cell), do: cell, else: nil
+  end
+
+  defp remove_nil_cells(cells) do
+    cells |> Enum.filter(fn cell -> cell != nil end)
+  end
+
+  def become_alive_tick(alive_cells) do
+    alive_cells
+    |> GameOfLife.Cell.dead_neighbours()
+    |> Enum.map(&(Task.Supervisor.async(
+      {GameOfLife.TaskSupervisor, GameOfLife.NodeManager.random_node},
+      GameOfLife.Board, :become_alive_or_nilify, [alive_cells, &1])))
+    |> Enum.map(&Task.await/1)
+    |> remove_nil_cells
+  end
+
+  def become_alive_or_nilify(alive_cells, dead_cell) do
+    if GameOfLife.Cell.become_alive?(alive_cells, dead_cell), do: dead_cell, else: nil
+  end
+end
---
+++ game_of_life/test/game_of_life/board_test.exs
@@ -0,0 +1,33 @@
+defmodule GameOfLife.BoardTest do
+  use ExUnit.Case, async: true
+
+  test "add new cells to alive cells without duplicates" do
+    alive_cells = [{1, 1}, {2, 2}]
+    new_cells = [{0, 0}, {1, 1}]
+    actual_alive_cells = GameOfLife.Board.add_cells(alive_cells, new_cells)
+                         |> Enum.sort
+    expected_alive_cells = [{0, 0}, {1, 1}, {2, 2}]
+    assert actual_alive_cells == expected_alive_cells
+  end
+
+  test "remove cells which must be killed from alive cells" do
+    alive_cells = [{1, 1}, {4, -2}, {2, 2}, {2, 1}]
+    kill_cells = [{1, 1}, {2, 2}]
+    actual_alive_cells = GameOfLife.Board.remove_cells(alive_cells, kill_cells)
+    expected_alive_cells = [{4, -2}, {2, 1}]
+    assert actual_alive_cells == expected_alive_cells
+  end
+
+  test "alive cell with 2 neighbours lives on to the next generation" do
+    alive_cells = [{0, 0}, {1, 0}, {2, 0}]
+    expected_alive_cells = [{1, 0}]
+    assert GameOfLife.Board.keep_alive_tick(alive_cells) == expected_alive_cells
+  end
+
+  test "dead cell with three live neighbours becomes a live cell" do
+    alive_cells = [{0, 0}, {1, 0}, {2, 0}, {1, 1}]
+    born_cells = GameOfLife.Board.become_alive_tick(alive_cells)
+    expected_born_cells = [{1, -1}, {0, 1}, {2, 1}]
+    assert born_cells == expected_born_cells
+  end
+end
