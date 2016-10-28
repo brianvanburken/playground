bac261c830d7c39e0f0e1a4505bd1c15c576c0f4
Brian van Burken <brianvanburken@outlook.com>
2016-10-28 11:06:57 +0200

    Add ability to define video offset

--- phoenix_video_stream/web/controllers/util.ex
+++ phoenix_video_stream/web/controllers/util.ex
@@ -6,8 +6,29 @@ defmodule PhoenixVideoStream.Util do
 
   def send_video(conn, headers, video) do
     video_path = build_video_path(video)
+    offset = get_offset(headers)
+    file_size = get_file_size(video_path)
+
     conn
     |> Plug.Conn.put_resp_header("content-type", video.content_type)
-    |> Plug.Conn.send_file(200, video_path)
+    |> Plug.Conn.put_resp_header("content-range", "bytes #{offset}-#{file_size-1}/#{file_size}")
+    |> Plug.Conn.send_file(206, video_path, offset, file_size - offset)
+  end
+
+  defp get_offset(headers) do
+    case List.keyfind(headers, "range", 0) do
+      {"range", "bytes="<> start_pos} ->
+        start_pos
+        |> String.split("-")
+        |> hd
+        |> String.to_integer
+      nil ->
+        0
+    end
+  end
+
+  defp get_file_size(path) do
+    {:ok, %{size: size}} = File.stat path
+    size
   end
 end
