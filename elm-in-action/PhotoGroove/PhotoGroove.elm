297c921ebc83a7702884e971bc974fdde5874c16
Brian van Burken <brianvanburken@outlook.com>
2017-03-08 11:19:13 +0100

    Render each thumbnail in a separate view

--- elm-in-action/PhotoGroove/PhotoGroove.elm
+++ elm-in-action/PhotoGroove/PhotoGroove.elm
@@ -4,16 +4,27 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
-            ]
+        , div [ id "thumbnails" ] (List.map viewThumbnail model)
         ]
 
 
+viewThumbnail thumbnail =
+    img [ src (urlPrefix ++ thumbnail.url) ] []
+
+
+initialModel =
+    [ { url = "1.jpeg" }
+    , { url = "2.jpeg" }
+    , { url = "3.jpeg" }
+    ]
+
+
 main =
-    view "no model yet"
+    view initialModel
