00a7db526f6deba69f5a3c858da59b19da16f62b
Brian van Burken <brianvanburken@outlook.com>
2017-03-10 16:19:03 +0100

    Elm in Action: Chapter 3.2

--- elm-in-action/PhotoGroove/PhotoGroove.elm
+++ elm-in-action/PhotoGroove/PhotoGroove.elm
@@ -3,30 +3,85 @@ module PhotoGroove exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Array exposing (Array)
 
 
+type ThumbnailSize
+    = Small
+    | Medium
+    | Large
+
+
+type Msg
+    = SelectByUrl String
+    | SupriseMe
+    | SetSize ThumbnailSize
+
+
+type alias Photo =
+    { url : String }
+
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    , chosenSize : ThumbnailSize
+    }
+
+
+urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
 
 
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
+        , button [ onClick SupriseMe ] [ text "Suprise Me!" ]
+        , h3 [] [ text "Thumbnail Size: " ]
+        , div [ id "choose-size" ]
+            (List.map viewSizeChooser [ Small, Medium, Large ])
+        , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
             (List.map (viewThumbnail model.selectedUrl) model.photos)
         , img [ class "large", src (urlPrefix ++ "large/" ++ model.selectedUrl) ] []
         ]
 
 
+viewThumbnail : String -> Photo -> Html Msg
 viewThumbnail selectedUrl thumbnail =
     img
         [ src (urlPrefix ++ thumbnail.url)
         , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
-        , onClick { operation = "SELECT_PHOTO", data = thumbnail.url }
+        , onClick (SelectByUrl thumbnail.url)
         ]
         []
 
 
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+    label []
+        [ input
+            [ type_ "radio", name "size", onClick (SetSize size) ]
+            []
+        , text (sizeToString size)
+        ]
+
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+    case size of
+        Small ->
+            "small"
+
+        Medium ->
+            "med"
+
+        Large ->
+            "large"
+
+
+initialModel : Model
 initialModel =
     { photos =
         [ { url = "1.jpeg" }
@@ -34,14 +89,26 @@ initialModel =
         , { url = "3.jpeg" }
         ]
     , selectedUrl = "1.jpeg"
+    , chosenSize = Medium
     }
 
 
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+
+update : Msg -> Model -> Model
 update msg model =
-    if msg.operation == "SELECT_PHOTO" then
-        { model | selectedUrl = msg.data }
-    else
-        model
+    case msg of
+        SelectByUrl url ->
+            { model | selectedUrl = url }
+
+        SupriseMe ->
+            { model | selectedUrl = "2.jpeg" }
+
+        SetSize size ->
+            { model | chosenSize = size }
 
 
 main =
