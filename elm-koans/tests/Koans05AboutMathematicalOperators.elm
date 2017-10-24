23fc9cceb3ed20542fd6a7fdd04ae72755c48ea7
Brian van Burken <brianvanburken+github@gmail.com>
2017-10-24 14:24:24 +0200

    Elm-koans 05

--- elm-koans/tests/Koans05AboutMathematicalOperators.elm
+++ elm-koans/tests/Koans05AboutMathematicalOperators.elm
@@ -9,58 +9,59 @@ testSuite =
     describe "About Mathematical Operators"
         [ test "negate gives the negative of a number" <|
             \() ->
-                negate (x____replace me____x)
+                negate (3)
                     |> Expect.equal -3
         , test "abs gives the absolute value of a number" <|
             \() ->
-                abs (x____replace me____x)
+                abs (-3)
                     |> Expect.equal 3
         , test "sqrt gives the square root of a Float" <|
             \() ->
-                sqrt (x____replace me____x)
+                sqrt (9)
                     |> Expect.equal 3.0
         , test "+ adds numbers" <|
             \() ->
-                1 + x____replace me____x
+                (1 + 2)
                     |> Expect.equal 3
         , test "- subtracts numbers" <|
             \() ->
-                7 - x____replace me____x
+                (7 - 4)
                     |> Expect.equal 3
         , test "* multiplies numbers" <|
             \() ->
-                4 * x____replace me____x
+                (4 * 1.5)
                     |> Expect.equal 6
         , test "/ divides Floats" <|
             \() ->
-                5 / x____replace me____x
+                (5 / 2)
                     |> Expect.equal 2.5
         , test "// divides Ints" <|
             \() ->
-                5 // x____replace me____x
+                (5 // 2)
                     |> Expect.equal 2
         , test "rem gives the remainder after division" <|
             \() ->
-                5 |> rem (x____replace me____x)
+                5
+                    |> rem (2)
                     |> Expect.equal 2
         , test "% performs modular arithmetic (which is different)" <|
             \() ->
-                x____replace me____x
+                4
                     |> Expect.equal (-1 % 5)
         , test "logBase returns the log of the value in the given base" <|
             \() ->
-                x____replace me____x
+                3
                     |> Expect.equal (logBase 2 8)
         , test "clamp returns the value if it is between the given min and max" <|
             \() ->
-                x____replace me____x
+                5
                     |> Expect.equal (clamp 1 10 5)
         , test "clamp returns the min value if the value is below the minimum" <|
             \() ->
-                x____replace me____x
+                1
                     |> Expect.equal (clamp 1 10 -5)
         , test "clamp returns the max value if the value is above the maximum" <|
             \() ->
-                x____replace me____x
+                10
                     |> Expect.equal (clamp 1 10 50)
         ]
