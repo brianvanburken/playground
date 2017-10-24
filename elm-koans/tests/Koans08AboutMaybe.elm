e194720eeebfc840ed44e3fe608e57993fe5b328
Brian van Burken <brianvanburken+github@gmail.com>
2017-10-24 14:47:06 +0200

    Elm-koans 08

--- elm-koans/tests/Koans08AboutMaybe.elm
+++ elm-koans/tests/Koans08AboutMaybe.elm
@@ -2,33 +2,32 @@ module Koans08AboutMaybe exposing (testSuite)
 
 import Expect
 import Utils.Test exposing (describe, test)
-import Utils.Blank exposing (..)
 
 
 testSuite =
     describe "About Maybe"
         [ test "maybe represents a value that may be nothing" <|
             \() ->
-                x____replace me____x
+                Nothing
                     |> Expect.equal Nothing
         , test "or the value could be something" <|
             \() ->
-                x____replace me____x
+                Just 5
                     |> Expect.equal (Just 5)
         , test "withDefault can be used to get the value from a maybe" <|
             \() ->
-                x____replace me____x
+                5
                     |> Expect.equal (Maybe.withDefault 3 (Just 5))
         , test "but will give the default value if there is nothing" <|
             \() ->
-                x____replace me____x
+                3
                     |> Expect.equal (Maybe.withDefault 3 Nothing)
         , test "map will transform the value in a maybe" <|
             \() ->
-                x____replace me____x
+                Just 2
                     |> Expect.equal (Maybe.map (\n -> n / 2) (Just 4))
         , test "but will not transform a nothing" <|
             \() ->
-                x____replace me____x
+                Nothing
                     |> Expect.equal (Maybe.map (\n -> n / 2) Nothing)
         ]
