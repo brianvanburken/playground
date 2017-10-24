9da76a6d1293e1fe02b59dc2bb529b8f6faf6f70
Brian van Burken <brianvanburken+github@gmail.com>
2017-10-24 14:43:39 +0200

    Elm-koans 07

--- elm-koans/tests/Koans07AboutFunctions.elm
+++ elm-koans/tests/Koans07AboutFunctions.elm
@@ -2,7 +2,6 @@ module Koans07AboutFunctions exposing (testSuite)
 
 import Expect
 import Utils.Test exposing (describe, test)
-import Utils.Blank exposing (..)
 
 
 add1 x =
@@ -39,50 +38,50 @@ testSuite =
     describe "About Functions"
         [ test "the identity function returns whatever it is passed" <|
             \() ->
-                x____replace me____x
+                4
                     |> Expect.equal (identity 4)
         , test "functions are defined as 'name args = implementation'" <|
             \() ->
-                add1 (x____replace me____x)
+                add1 (4)
                     |> Expect.equal 5
         , test "functions may have an optional type signature" <|
             \() ->
-                add 3 (x____replace me____x)
+                add 3 (2)
                     |> Expect.equal 5
         , test "anonymous functions are defined with '\\args -> implementation'" <|
             \() ->
-                subtract 8 (x____replace me____x)
+                subtract 8 (3)
                     |> Expect.equal 5
         , test "anonymous functions may be defined inline" <|
             \() ->
-                (\x y -> x - y) 8 (x____replace me____x)
+                (\x y -> x - y) 8 (3)
                     |> Expect.equal 5
         , test "functions passed some values return curried functions" <|
             \() ->
-                subtractFrom4 (x____replace me____x)
+                subtractFrom4 (3)
                     |> Expect.equal 1
         , test "the always function returns its first argument, no matter what the second is" <|
             \() ->
-                x____replace me____x
+                5
                     |> Expect.equal (always5 4)
         , test "the flip function flips the order of the first two arguments of a function" <|
             \() ->
-                x____replace me____x
+                5
                     |> Expect.equal (subtract4 9)
         , test "f <| a applies the function f to the arg a" <|
             \() ->
-                x____replace me____x
+                6
                     |> Expect.equal (subtract4 <| add 1 9)
         , test "a |> f applies the function f to the arg a" <|
             \() ->
-                x____replace me____x
+                6
                     |> Expect.equal (add 1 9 |> subtract4)
         , test "g << f composes function g with function f" <|
             \() ->
-                x____replace me____x
+                10
                     |> Expect.equal ((multiplyBy2 << subtract4) 9)
         , test "f >> g composes function g with function f" <|
             \() ->
-                x____replace me____x
+                10
                     |> Expect.equal ((subtract4 >> multiplyBy2) 9)
         ]
