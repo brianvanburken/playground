218641c6160f875d3f472474a279a1c3c03c77cf
Brian van Burken <brianvanburken+github@gmail.com>
2017-10-24 14:38:08 +0200

    Elm-koans 06

--- elm-koans/tests/Koans06AboutNumberConversions.elm
+++ elm-koans/tests/Koans06AboutNumberConversions.elm
@@ -2,45 +2,44 @@ module Koans06AboutNumberConversions exposing (testSuite)
 
 import Expect
 import Utils.Test exposing (describe, test)
-import Utils.Blank exposing (..)
 
 
 testSuite =
     describe "About Number Conversions"
         [ test "toFloat converts an int to a float" <|
             \() ->
-                x____replace me____x
+                5.0
                     |> Expect.equal (toFloat 5)
         , test "floor converts a float to an int (rounding down)" <|
             \() ->
-                x____replace me____x
+                3
                     |> Expect.equal (floor 3.6)
-        , test "floor converts a float to an int (rounding down)" <|
+        , test "floor converts a negative float to an int (rounding down)" <|
             \() ->
-                x____replace me____x
+                -4
                     |> Expect.equal (floor -3.6)
         , test "ceiling converts a float to an int (rounding up)" <|
             \() ->
-                x____replace me____x
+                4
                     |> Expect.equal (ceiling 3.6)
-        , test "ceiling converts a float to an int (rounding up)" <|
+        , test "ceiling converts a negative float to an int (rounding up)" <|
             \() ->
-                x____replace me____x
+                -3
                     |> Expect.equal (ceiling -3.6)
         , test "round converts a float to an int (rounding to the closest int)" <|
             \() ->
-                x____replace me____x
+                4
                     |> Expect.equal (round 3.6)
-        , test "round converts a float to an int (rounding to the closest int)" <|
+        , test "round converts a negative float to an int (rounding to the closest int)" <|
             \() ->
-                x____replace me____x
+                -4
                     |> Expect.equal (round -3.6)
         , test "truncate converts a float to an int (rounding towards 0)" <|
             \() ->
-                x____replace me____x
+                3
                     |> Expect.equal (truncate 3.6)
-        , test "truncate converts a float to an int (rounding towards 0)" <|
+        , test "truncate converts a negative float to an int (rounding towards 0)" <|
             \() ->
-                x____replace me____x
+                -3
                     |> Expect.equal (truncate -3.6)
         ]
