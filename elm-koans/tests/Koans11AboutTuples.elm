e0a6c8b5007b833680c50add364d7adcea102f3b
Brian van Burken <brianvanburken+github@gmail.com>
2017-10-24 15:28:13 +0200

    Elm-koans 11

--- elm-koans/tests/Koans11AboutTuples.elm
+++ elm-koans/tests/Koans11AboutTuples.elm
@@ -2,7 +2,6 @@ module Koans11AboutTuples exposing (testSuite)
 
 import Expect
 import Utils.Test exposing (describe, test)
-import Utils.Blank exposing (..)
 import Tuple
 
 
@@ -10,37 +9,37 @@ testSuite =
     describe "About Tuples"
         [ test "tuples are like lists of fixed length" <|
             \() ->
-                x____replace me____x
+                ( 1, 2 )
                     |> Expect.equal ( 1, 2 )
         , test "tuples may also be of mixed types" <|
             \() ->
-                x____replace me____x
+                ( 1, "hey" )
                     |> Expect.equal ( 1, "hey" )
         , test "there is a special comma syntax for creating tuples" <|
             \() ->
-                x____replace me____x
+                ( 1, "hey" )
                     |> Expect.equal ((,) 1 "hey")
         , test "you use as many commas as there would be in the tuple" <|
             \() ->
-                x____replace me____x
+                ( 1, "hey", [] )
                     |> Expect.equal ((,,) 1 "hey" [])
         , test "first gets the first element of a 2-tuple" <|
             \() ->
-                x____replace me____x
-                    |> Expect.equal (Tuple.first (x____replace me____x))
+                1
+                    |> Expect.equal (Tuple.first ( 1, 2 ))
         , test "second gets the second element of a 2-tuple" <|
             \() ->
-                x____replace me____x
-                    |> Expect.equal (Tuple.second (x____replace me____x))
+                2
+                    |> Expect.equal (Tuple.second ( 1, 2 ))
         , test "case statements may be used to destructure a tuple" <|
             \() ->
                 case ( 1, 2 ) of
                     ( first, second ) ->
-                        first == x____replace me____x && second == x____replace me____x
+                        (first == 1 && second == 2)
                             |> Expect.true "Should be True"
         , test "tuples may also be destructured by function arguments" <|
             \() ->
                 ( 1, 2 )
-                    |> (\( f, s ) -> f == x____replace me____x && s == x____replace me____x)
+                    |> (\( f, s ) -> f == 1 && s == 2)
                     |> Expect.true "Should be True"
         ]
