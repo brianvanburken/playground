d8daebb61282619b336c12fe3d86ba5a59a92a4b
Brian van Burken <brianvanburken+github@gmail.com>
2017-10-25 08:18:11 +0200

    Elm-koans 14

--- elm-koans/tests/Koans14AboutStrings.elm
+++ elm-koans/tests/Koans14AboutStrings.elm
@@ -10,71 +10,71 @@ testSuite =
     describe "About Strings"
         [ test "length returns the string length" <|
             \() ->
-                x____replace me____x
+                5
                     |> Expect.equal (String.length "Hello")
         , test "isEmpty tests for an empty string" <|
             \() ->
-                String.isEmpty (x____replace me____x)
+                String.isEmpty ("")
                     |> Expect.true "Should be True"
         , test "reverse returns the reverse of a string" <|
             \() ->
-                x____replace me____x
+                "olleH"
                     |> Expect.equal (String.reverse "Hello")
         , test "repeat returns the string repeated n times" <|
             \() ->
-                x____replace me____x
+                "yaya"
                     |> Expect.equal (String.repeat 2 "ya")
         , test "the ++ infix function appends two strings" <|
             \() ->
-                x____replace me____x
+                "ElmLang"
                     |> Expect.equal ("Elm" ++ "Lang")
         , test "append does the same thing" <|
             \() ->
-                x____replace me____x
+                "ElmLang"
                     |> Expect.equal (String.append "Elm" "Lang")
         , test "concat appends a list of strings together" <|
             \() ->
-                x____replace me____x
+                "ElmLangYay!"
                     |> Expect.equal (String.concat [ "Elm", "Lang", "Yay!" ])
         , test "join is like concat with a separator" <|
             \() ->
-                x____replace me____x
+                "I'm,a,csv"
                     |> Expect.equal (String.join "," [ "I'm", "a", "csv" ])
         , test "split does the opposite" <|
             \() ->
-                x____replace me____x
+                [ "I'm", "a", "csv" ]
                     |> Expect.equal (String.split "," "I'm,a,csv")
         , test "slice gets a substring" <|
             \() ->
-                x____replace me____x
+                "a string"
                     |> Expect.equal (String.slice 4 12 "I'm a string")
         , test "negative indices start from the end" <|
             \() ->
-                x____replace me____x
+                "a strin"
                     |> Expect.equal (String.slice -8 -1 "I'm a string")
         , test "left takes the first n characters of a string" <|
             \() ->
-                x____replace me____x
+                "I'm"
                     |> Expect.equal (String.left 3 "I'm a string")
         , test "right takes the last n characters of a string" <|
             \() ->
-                x____replace me____x
+                "ing"
                     |> Expect.equal (String.right 3 "I'm a string")
         , test "dropLeft drops the first n characters of a string" <|
             \() ->
-                x____replace me____x
+                " a string"
                     |> Expect.equal (String.dropLeft 3 "I'm a string")
         , test "dropRight drops the last n characters of a string" <|
             \() ->
-                x____replace me____x
+                "I'm a str"
                     |> Expect.equal (String.dropRight 3 "I'm a string")
         , test "contains looks for a substring" <|
             \() ->
-                x____replace me____x
+                True
                     |> Expect.equal (String.contains "string" "I'm a string")
         , test "indexes gives the location of each occurance" <|
             -- there are sooooo many string functions, just check them out
             \() ->
-                x____replace me____x
+                [ 6 ]
                     |> Expect.equal (String.indexes "string" "I'm a string")
         ]
