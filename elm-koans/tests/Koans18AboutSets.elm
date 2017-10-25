9a6979771c6c83c90d495cb06a892f26ca698d84
Brian van Burken <brianvanburken+github@gmail.com>
2017-10-25 08:42:06 +0200

    Elm-koans 18

--- elm-koans/tests/Koans18AboutSets.elm
+++ elm-koans/tests/Koans18AboutSets.elm
@@ -18,50 +18,50 @@ testSuite =
     describe "About Sets"
         [ test "a set can be created from a list" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 1, 2, 3 ]
                     |> assertEqualSets (Set.fromList [ 1, 2, 3 ])
         , test "a set cannot contain duplicates" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 1, 2, 3 ]
                     |> assertEqualSets (Set.fromList [ 1, 2, 3, 3, 2, 1 ])
         , test "sets may be empty" <|
             \() ->
-                x____replace me____x
+                Set.empty
                     |> assertEqualSets Set.empty
         , test "or contain a single value" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 0 ]
                     |> assertEqualSets (Set.singleton 0)
         , test "insert may add a new element to a set" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 1, 2, 3, 4 ]
                     |> assertEqualSets (Set.insert 4 oneTwoThree)
         , test "but may not add duplicates" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 1, 2, 3 ]
                     |> assertEqualSets (Set.insert 1 oneTwoThree)
         , test "remove may subtract an element from a set" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 2, 3 ]
                     |> assertEqualSets (Set.remove 1 oneTwoThree)
         , test "but only if it is there" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 1, 2, 3 ]
                     |> assertEqualSets (Set.remove 4 oneTwoThree)
         , test "member can check if an element is in a set" <|
             \() ->
-                x____replace me____x
+                True
                     |> Expect.equal (Set.member 2 oneTwoThree)
         , test "union will add two sets" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 1, 2, 3, 4 ]
                     |> assertEqualSets (Set.union oneTwoThree (Set.fromList [ 3, 4 ]))
         , test "intersect will get the intersection" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 3 ]
                     |> assertEqualSets (Set.intersect oneTwoThree (Set.fromList [ 3, 4 ]))
         , test "diff is the difference between the first and second sets" <|
             \() ->
-                x____replace me____x
+                Set.fromList [ 2 ]
                     |> assertEqualSets (Set.diff oneTwoThree (Set.fromList [ 1, 3 ]))
         ]
