00191d671b63f823c1de9979fe03a83b78c386a5
Brian van Burken <brianvanburken@gmail.com>
2018-05-14 09:35:49 +0200

    Add test with zips

---
+++ zips/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const archiver = require('archiver');
+
+// create a file to stream archive data to.
+const output = fs.createWriteStream(__dirname + '/output.zip');
+const archive = archiver('zip', {
+  zlib: { level: 9 } // Sets the compression level.
+});
+
+output.on('close', function() {
+  console.log(archive.pointer() + ' total bytes');
+  console.log('archiver has been finalized and the output file descriptor has closed.');
+});
+
+archive.on('error', function(err) {
+  throw err;
+});
+
+// pipe archive data to the file
+archive.pipe(output);
+archive.append('Hello world!', { name: 'hello_world.txt' });
+archive.finalize();
