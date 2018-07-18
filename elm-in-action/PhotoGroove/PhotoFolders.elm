74a7ef7cef6b256c7a87ba471d4c3ef1d25fb6cb
Brian van Burken <brianvanburken@gmail.com>
2018-07-18 15:26:13 +0200

    Add Elm in Action chapter 7.2

--- elm-in-action/PhotoGroove/PhotoFolders.elm
+++ elm-in-action/PhotoGroove/PhotoFolders.elm
@@ -17,9 +17,24 @@ type alias Photo =
     }
 
 
+type FolderPath
+    = End
+    | Subfolder Int FolderPath
+
+
+type Folder
+    = Folder
+        { name : String
+        , photoUrls : List String
+        , subfolders : List Folder
+        , expanded : Bool
+        }
+
+
 type alias Model =
     { selectedPhotoUrl : Maybe String
     , photos : Dict String Photo
+    , root : Folder
     }
 
 
@@ -27,6 +42,7 @@ initialModel : Model
 initialModel =
     { selectedPhotoUrl = Nothing
     , photos = Dict.empty
+    , root = Folder { name = "Loading...", photoUrls = [], subfolders = [], expanded = True }
     }
 
 
@@ -49,18 +65,45 @@ modelDecoder =
                 , ( "fresco", { title = "Fresco", relatedUrls = [ "trevi" ], size = 46, url = "fresco" } )
                 , ( "coli", { title = "Coliseum", relatedUrls = [ "trevi", "fresco" ], size = 36, url = "coli" } )
                 ]
+        , root =
+            Folder
+                { name = "Photos"
+                , photoUrls = []
+                , expanded = True
+                , subfolders =
+                    [ Folder
+                        { name = "2016"
+                        , photoUrls = [ "trevi", "coli" ]
+                        , expanded = True
+                        , subfolders =
+                            [ Folder { name = "outdoors", expanded = True, photoUrls = [], subfolders = [] }
+                            , Folder { name = "indoorrs", expanded = True, photoUrls = [ "fresco" ], subfolders = [] }
+                            ]
+                        }
+                    , Folder
+                        { name = "2017"
+                        , photoUrls = []
+                        , expanded = True
+                        , subfolders =
+                            [ Folder { name = "outdoors", expanded = True, photoUrls = [], subfolders = [] }
+                            , Folder { name = "indoorrs", expanded = True, photoUrls = [], subfolders = [] }
+                            ]
+                        }
+                    ]
+                }
         }
 
 
 type Msg
-    = SelectedPhotoUrl String
+    = SelectPhotoUrl String
     | LoadPage (Result Http.Error Model)
+    | ToggleExpanded FolderPath
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        SelectedPhotoUrl url ->
+        SelectPhotoUrl url ->
             ( { model | selectedPhotoUrl = Just url }, Cmd.none )
 
         LoadPage (Ok newModel) ->
@@ -68,6 +111,9 @@ update msg model =
 
         LoadPage (Err _) ->
             ( model, Cmd.none )
+
+        ToggleExpanded path ->
+            ( { model | root = toggleExpanded path model.root }, Cmd.none )
 
 
 view : Model -> Html Msg
@@ -87,7 +133,14 @@ view model =
                     text ""
     in
     div [ class "content" ]
-        [ div [ class "selected-photo" ] [ selectedPhoto ] ]
+        [ div [ class "folders" ]
+            [ h1 [] [ text "Folders" ]
+            , viewFolder End model.root
+            ]
+        , div
+            [ class "selected-photo" ]
+            [ selectedPhoto ]
+        ]
 
 
 main : Program Never Model Msg
@@ -98,6 +151,12 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+viewPhoto : String -> Html Msg
+viewPhoto url =
+    div [ class "photo", onClick (SelectPhotoUrl url) ]
+        [ text url ]
 
 
 viewSelectedPhoto : Photo -> Html Msg
@@ -116,12 +175,71 @@ viewReleatedPhoto : String -> Html Msg
 viewReleatedPhoto url =
     img
         [ class "related-photo"
-        , onClick (SelectedPhotoUrl url)
+        , onClick (SelectPhotoUrl url)
         , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
         ]
         []
 
 
+viewFolder : FolderPath -> Folder -> Html Msg
+viewFolder path (Folder folder) =
+    let
+        viewSubfolder : Int -> Folder -> Html Msg
+        viewSubfolder index subfolder =
+            viewFolder (appendIndex index path) subfolder
+
+        folderLabel =
+            label [ onClick (ToggleExpanded path) ] [ text folder.name ]
+    in
+    if folder.expanded then
+        let
+            contents =
+                List.append
+                    (List.indexedMap viewSubfolder folder.subfolders)
+                    (List.map viewPhoto folder.photoUrls)
+        in
+        div [ class "folder expanded" ]
+            [ folderLabel
+            , div [ class "contents" ] contents
+            ]
+
+    else
+        div [ class "folder collapsed" ] [ folderLabel ]
+
+
 urlPrefix : String
 urlPrefix =
     "http://elm-in-action.com/"
+
+
+toggleExpanded : FolderPath -> Folder -> Folder
+toggleExpanded path (Folder folder) =
+    case path of
+        End ->
+            Folder { folder | expanded = not folder.expanded }
+
+        Subfolder targetIndex remainingPath ->
+            let
+                subfolders : List Folder
+                subfolders =
+                    List.indexedMap transform folder.subfolders
+
+                transform : Int -> Folder -> Folder
+                transform currentIndex currentSubfolder =
+                    if currentIndex == targetIndex then
+                        toggleExpanded remainingPath currentSubfolder
+
+                    else
+                        currentSubfolder
+            in
+            Folder { folder | subfolders = subfolders }
+
+
+appendIndex : Int -> FolderPath -> FolderPath
+appendIndex index path =
+    case path of
+        End ->
+            Subfolder index End
+
+        Subfolder subfolderIndex remainingPath ->
+            Subfolder subfolderIndex (appendIndex index remainingPath)
