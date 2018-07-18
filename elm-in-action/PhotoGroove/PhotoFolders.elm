2d1165806ceb2d906ed469e1010e1f8e28e2b105
Brian van Burken <brianvanburken@gmail.com>
2018-07-18 14:30:58 +0200

    Add Elm in Action chapter 7.1

---
+++ elm-in-action/PhotoGroove/PhotoFolders.elm
@@ -0,0 +1,127 @@
+module PhotoFolders exposing (main)
+
+import Dict exposing (Dict)
+import Html exposing (..)
+import Html.Attributes exposing (class, src)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (required)
+
+
+type alias Photo =
+    { title : String
+    , size : Int
+    , relatedUrls : List String
+    , url : String
+    }
+
+
+type alias Model =
+    { selectedPhotoUrl : Maybe String
+    , photos : Dict String Photo
+    }
+
+
+initialModel : Model
+initialModel =
+    { selectedPhotoUrl = Nothing
+    , photos = Dict.empty
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel
+    , modelDecoder
+        |> Http.get "http://elm-in-action.com/folders/list"
+        |> Http.send LoadPage
+    )
+
+
+modelDecoder : Decoder Model
+modelDecoder =
+    Decode.succeed
+        { selectedPhotoUrl = Just "trevi"
+        , photos =
+            Dict.fromList
+                [ ( "trevi", { title = "Trevi", relatedUrls = [ "coli", "fresco" ], size = 34, url = "trevi" } )
+                , ( "fresco", { title = "Fresco", relatedUrls = [ "trevi" ], size = 46, url = "fresco" } )
+                , ( "coli", { title = "Coliseum", relatedUrls = [ "trevi", "fresco" ], size = 36, url = "coli" } )
+                ]
+        }
+
+
+type Msg
+    = SelectedPhotoUrl String
+    | LoadPage (Result Http.Error Model)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SelectedPhotoUrl url ->
+            ( { model | selectedPhotoUrl = Just url }, Cmd.none )
+
+        LoadPage (Ok newModel) ->
+            ( newModel, Cmd.none )
+
+        LoadPage (Err _) ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    let
+        photoByUrl : String -> Maybe Photo
+        photoByUrl url =
+            Dict.get url model.photos
+
+        selectedPhoto : Html Msg
+        selectedPhoto =
+            case Maybe.andThen photoByUrl model.selectedPhotoUrl of
+                Just photo ->
+                    viewSelectedPhoto photo
+
+                Nothing ->
+                    text ""
+    in
+    div [ class "content" ]
+        [ div [ class "selected-photo" ] [ selectedPhoto ] ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        }
+
+
+viewSelectedPhoto : Photo -> Html Msg
+viewSelectedPhoto photo =
+    div [ class "selected-photo" ]
+        [ h2 [] [ text photo.title ]
+        , img [ src (urlPrefix ++ "photos/" ++ photo.url ++ "/full") ] []
+        , span [] [ text (toString photo.size ++ "KB") ]
+        , h3 [] [ text "Related" ]
+        , div [ class "related-photos" ]
+            (List.map viewReleatedPhoto photo.relatedUrls)
+        ]
+
+
+viewReleatedPhoto : String -> Html Msg
+viewReleatedPhoto url =
+    img
+        [ class "related-photo"
+        , onClick (SelectedPhotoUrl url)
+        , src (urlPrefix ++ "photos/" ++ url ++ "/thumb")
+        ]
+        []
+
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
