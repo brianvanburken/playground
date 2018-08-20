8ab84ff9460f2f336162191d1a62abacc1bc64e2
Brian van Burken <brianvanburken@gmail.com>
2018-08-20 09:27:58 +0200

    Add step 8 HTTP of the tutorial

---
+++ angular-tour-of-heroes/src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css']
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit() {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+}
