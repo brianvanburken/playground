052919bc3048e3fbb10d5c5bbcdca4dca3c45dc0
Brian van Burken <brianvanburken@gmail.com>
2018-09-09 12:29:23 +0200

    Simplified function

--- lambda/greetingsOnDemand.js
+++ lambda/greetingsOnDemand.js
@@ -1,16 +1,7 @@
-console.log('Loading function');
-
 exports.handler = (event, context, callback) => {
-    console.log('Received event:',
-        JSON.stringify(event, null, 2));
-    console.log('name =', event.name);
-    var name = '';
-    if ('name' in event) {
-        name = event['name'];
-    } else {
-        name = "World";
-    }
-    var greetings = 'Hello ' + name + '!';
-    console.log(greetings);
+    const name = 'name' in event
+        ?  event['name']
+        : 'World';
+    const greetings = 'Hello ' + name + '!';
     callback(null, greetings);
 };
