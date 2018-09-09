31806ce63fb36cb6bef0be0b326e01ff80c78207
Brian van Burken <brianvanburken@gmail.com>
2018-09-09 12:33:18 +0200

    Add env variable to function

--- lambda/greetingsOnDemand.js
+++ lambda/greetingsOnDemand.js
@@ -2,6 +2,9 @@ exports.handler = (event, context, callback) => {
     const name = 'name' in event
         ?  event['name']
         : 'World';
-    const greetings = 'Hello ' + name + '!';
+    const greeting = process.env.GREETING
+        ? process.env.GREETING
+        : 'Hello'
+    const greetings = greeting + ' ' + name + '!';
     callback(null, greetings);
 };
