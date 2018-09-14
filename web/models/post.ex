0369036fd641eca278312c8cc99493020935f480
Brian van Burken <brianvanburken@gmail.com>
2018-09-14 10:33:46 +0200

    Revert "Add phoenix_blog repository"
    This reverts commit d5411b5e2e5bb26633783fdaca9d02aa35595db0.

---
+++ web/models/post.ex
@@ -0,0 +1,33 @@
+defmodule BlogPhoenix.Post do
+  use BlogPhoenix.Web, :model
+
+  schema "posts" do
+    field :title, :string
+    field :body, :string
+
+    has_many :comments, BlogPhoenix.Comment
+
+    timestamps
+  end
+
+  @required_fields ~w(title body)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+
+  def count_comments(query) do
+    from p in query,
+      group_by: p.id,
+      left_join: c in assoc(p, :comments),
+      select: {p, count(c.id)}
+  end
+end
