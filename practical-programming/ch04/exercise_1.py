11e5700470280c8fc266ad89e1b47c051927a2fc
Brian van Burken <brianvanburken@gmail.com>
2019-09-24 16:05:30 +0200

    Add chapter 4

---
+++ practical-programming/ch04/exercise_1.py
@@ -0,0 +1,4 @@
+'Computer' + 'Science' == 'ComputerScience'
+'Darwin\'s' == "Darwin's"  # ' is escaped
+'H2O' * 3 == 'H2OH2OH2O'
+'CO2' * 0 == ''
---
+++ practical-programming/ch04/exercise_5.py
@@ -0,0 +1,8 @@
+x = 3
+y = 12.5
+
+print('The rabbit is ', x, '.')
+print('The rabbit is ', x, ' years old.')
+print(y, ' is average.')
+print(y, ' * ', x)
+print(y, ' * ', x, ' is ', y * x, '.')
