772383993023c9081793ab36398f33cb86d7336b
Brian van Burken <brianvanburken@gmail.com>
2020-10-24 10:59:12 +0200

    Elm in Action: chapter 2.2

--- elm/elm-in-action/PhotoGroove/src/PhotoGroove.elm
+++ elm/elm-in-action/PhotoGroove/src/PhotoGroove.elm
@@ -1,21 +1,59 @@
 module PhotoGroove exposing (main)
 
+import Browser
 import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
-view : a -> Html msg
 view model =
     div [ class "content" ]
         [ h1 [] [ text "Photo Groove" ]
         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+            (List.map (viewThumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
+            []
         ]
 
 
-main : Html msg
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+
+viewThumbnail selectedUrl thumb =
+    img
+        [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url }
+        ]
+        []
+
+
+initialModel =
+    { photos =
+        [ { url = "1.jpeg" }
+        , { url = "2.jpeg" }
+        , { url = "3.jpeg" }
+        ]
+    , selectedUrl = "1.jpeg"
+    }
+
+
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data }
+
+    else
+        model
+
+
 main =
-    view "no model yet"
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
