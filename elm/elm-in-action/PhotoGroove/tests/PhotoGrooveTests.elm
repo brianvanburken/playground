ccf70d53892de2d89e075248f772d4084fa05e8e
Brian van Burken <brianvanburken@gmail.com>
2020-10-27 19:54:24 +0100

    Elm in Action: chapter 6.2

--- elm/elm-in-action/PhotoGroove/tests/PhotoGrooveTests.elm
+++ elm/elm-in-action/PhotoGroove/tests/PhotoGrooveTests.elm
@@ -1,16 +1,41 @@
 module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
-import Json.Decode exposing (decodeString)
-import PhotoGroove
+import Fuzz exposing (Fuzzer, int, list, string)
+import Json.Decode as Decode exposing (decodeValue)
+import Json.Encode as Encode
+import PhotoGroove exposing (Model, Msg(..), Photo, initialModel, update)
 import Test exposing (..)
 
 
 decoderTest : Test
 decoderTest =
-    test "title defaults to (untitled)" <|
-        \_ ->
-            """{"url": "fruits.com", "size": 5}"""
-                |> decodeString PhotoGroove.photoDecoder
+    fuzz2 string int "title defaults to (untitled)" <|
+        \url size ->
+            [ ( "url", Encode.string url )
+            , ( "size", Encode.int size )
+            ]
+                |> Encode.object
+                |> decodeValue PhotoGroove.photoDecoder
                 |> Result.map .title
                 |> Expect.equal (Ok "(untitled)")
+
+
+sliders : Test
+sliders =
+    describe "Slider sets the desired field in the Model"
+        [ testSlider "SlidHue" SlidHue .hue
+        , testSlider "SlidRipple" SlidRipple .ripple
+        , testSlider "SlidNoise" SlidNoise .noise
+        ]
+
+
+testSlider : String -> (Int -> Msg) -> (Model -> Int) -> Test
+testSlider description toMsg amountFromModel =
+    fuzz int description <|
+        \amount ->
+            initialModel
+                |> update (toMsg amount)
+                |> Tuple.first
+                |> amountFromModel
+                |> Expect.equal amount
