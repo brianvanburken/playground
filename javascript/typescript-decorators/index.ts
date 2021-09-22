b1dbaa460b7861e4ef0a20b22f52d78e929d711d
Brian van Burken <brianvanburken@users.noreply.github.com>
2021-09-22 09:42:10 +0200

    Create an example guard DSL with decorators

--- javascript/typescript-decorators/index.ts
+++ javascript/typescript-decorators/index.ts
@@ -1,29 +1,59 @@
-const NonEmtpyString = (
-  target: Object,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-) => {
-  const originalMethod = descriptor.value;
+function isNonEmptyString(a: string): boolean {
+  return a !== "";
+}
 
-  descriptor.value = function (...args: unknown[]) {
-    if (args[0] === "") {
-      console.log("String is empty");
-    } else {
-      originalMethod.apply(this, args);
-    }
+function minLength(a: string, len: number): boolean {
+  return a.length >= len;
+}
+
+function arg<T>(
+  index: number,
+  fn: (a: T) => boolean
+): (args: unknown[]) => boolean {
+  return function (args: unknown[]): boolean {
+    return fn(args?.[index] as T);
   };
+}
 
-  return descriptor;
-};
+function args<F extends (...args: any) => boolean>(
+  fn: F
+): (args: unknown[]) => boolean {
+  return function (args: unknown[]): boolean {
+    return fn(...args);
+  };
+}
+
+type ArgFunction = (args: unknown[]) => boolean;
+
+function Guard(...validators: ArgFunction[]) {
+  return (
+    _target: Object,
+    _propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function (...args: unknown[]) {
+      if (validators.every((fn) => fn.bind(this)(args))) {
+        originalMethod.apply(this, args);
+      } else {
+        console.info("Guard did not pass");
+      }
+    };
+
+    return descriptor;
+  };
+}
 
 class Check {
   public test = false;
 
-  @NonEmtpyString
-  public static checkString(str: string): void {
-    console.log("String is not empty");
+  @Guard(arg<string>(0, isNonEmptyString), args(minLength))
+  public static staticCheckString(str: string, len: number): void {
+    console.log(`String is not empty, "${str}", and is of length ${len}`);
   }
 }
 
-Check.checkString("");
-Check.checkString("non-empty");
+Check.staticCheckString("", 99);
+Check.staticCheckString("non-empty", 99);
+Check.staticCheckString("non-empty string of length", 10);
