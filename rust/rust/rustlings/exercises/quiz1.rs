277774f60b2c83d74769998f8dba7d1187a0b31f
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-08 09:18:10 +0200

    feat: rustlings quiz 1

--- rust/rust/rustlings/exercises/quiz1.rs
+++ rust/rust/rustlings/exercises/quiz1.rs
@@ -8,10 +8,14 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_apple_price {
+fn calculate_apple_price(amount_of_apples: i32) -> i32 {
+    if (amount_of_apples > 40) {
+        amount_of_apples * 1
+    } else {
+        amount_of_apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
