b79cab3636e34ddb45c6616f6b8c886289c6a4e0
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-07-11 15:55:17 +0200

    feat: rustlings advanced errs 2

--- rust/rust/rustlings/exercises/advanced_errors/advanced_errs2.rs
+++ rust/rust/rustlings/exercises/advanced_errors/advanced_errs2.rs
@@ -16,8 +16,6 @@
 // 4. Complete the partial implementation of `Display` for
 //    `ParseClimateError`.
 
-// I AM NOT DONE
-
 use std::error::Error;
 use std::fmt::{self, Display, Formatter};
 use std::num::{ParseFloatError, ParseIntError};
@@ -46,12 +44,13 @@ impl From<ParseIntError> for ParseClimateError {
 // `ParseFloatError` values.
 impl From<ParseFloatError> for ParseClimateError {
     fn from(e: ParseFloatError) -> Self {
-        // TODO: Complete this function
+        Self::ParseFloat(e)
     }
 }
 
 // TODO: Implement a missing trait so that `main()` below will compile. It
 // is not necessary to implement any methods inside the missing trait.
+impl Error for ParseClimateError {}
 
 // The `Display` trait allows for other code to obtain the error formatted
 // as a user-visible string.
@@ -62,7 +61,10 @@ impl Display for ParseClimateError {
         // Imports the variants to make the following code more compact.
         use ParseClimateError::*;
         match self {
+            Empty => write!(f, "empty input"),
+            BadLen => write!(f, "incorrect number of fields"),
             NoCity => write!(f, "no city name"),
+            ParseInt(e) => write!(f, "error parsing year: invalid digit found in string"),
             ParseFloat(e) => write!(f, "error parsing temperature: {}", e),
         }
     }
@@ -88,8 +90,12 @@ impl FromStr for Climate {
     // TODO: Complete this function by making it handle the missing error
     // cases.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let v: Vec<_> = s.split(',').collect();
+        if s.trim().len() == 0 {
+            return Err(ParseClimateError::Empty);
+        }
+        let v: Vec<_> = s.split(',').map(|w| w.trim()).collect();
         let (city, year, temp) = match &v[..] {
+            ["", ..] => return Err(ParseClimateError::NoCity),
             [city, year, temp] => (city.to_string(), year, temp),
             _ => return Err(ParseClimateError::BadLen),
         };
