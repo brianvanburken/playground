dfaf56f6c2fbe66d6eeb3891e0de4905dfe129e3
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 20:27:53 +0100

    feat: complete activity 07

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a7.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a7.rs
@@ -10,4 +10,20 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+fn main() {
+    display_color(Color::Red);
+}
+
+fn display_color(color: Color) {
+    match color {
+        Color::Red => println!("red"),
+        Color::Green => println!("green"),
+        Color::Blue => println!("blue"),
+    }
+}
