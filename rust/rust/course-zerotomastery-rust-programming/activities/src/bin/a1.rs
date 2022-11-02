57668e6ff3a4241a498a7a5ea487abdfe29d25ce
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 19:19:09 +0100

    feat: complete activity 01

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a1.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a1.rs
@@ -8,4 +8,14 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    first_name();
+    last_name();
+}
+
+fn first_name() {
+    println!("Brian")
+}
+fn last_name() {
+    println!("van Burken")
+}
