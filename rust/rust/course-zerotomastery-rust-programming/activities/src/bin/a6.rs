a76555704e99268254d710a63334331ceacda357
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 20:24:09 +0100

    feat: complete activity 06

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a6.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a6.rs
@@ -10,4 +10,11 @@
 // * Print the variable within the while loop
 // * Do not use break to exit the loop
 
-fn main() {}
+fn main() {
+    let mut n = 5;
+    while n > 0 {
+        println!("{:?}", n);
+        n = n - 1;
+    }
+    println!("done");
+}
