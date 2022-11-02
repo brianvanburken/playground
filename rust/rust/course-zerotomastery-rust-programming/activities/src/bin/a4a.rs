d2156e0a7efd0501cd1913bb091182d504581d31
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 20:18:55 +0100

    feat: complete activity 04a

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a4a.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a4a.rs
@@ -7,4 +7,10 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+    let bool = true;
+    match bool {
+        true => println!("it's true"),
+        false => println!("it's false"),
+    }
+}
