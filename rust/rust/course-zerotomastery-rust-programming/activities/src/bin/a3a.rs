8964e043a32e942b0611aec298985deecf81d97f
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 19:52:32 +0100

    feat: complete activity 03a

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a3a.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a3a.rs
@@ -10,4 +10,12 @@
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let is_greeting = false;
+
+    if is_greeting {
+        println!("hello")
+    } else {
+        println!("goodbye")
+    }
+}
