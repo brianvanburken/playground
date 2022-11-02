f1038b96eeb02953d8b74e582b3783f65d508db3
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 20:19:57 +0100

    feat: complete activity 04b

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a4b.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a4b.rs
@@ -10,4 +10,12 @@
 // * Use a match expression to determine which message to display
 // * Use an underscore (_) to match on any value
 
-fn main() {}
+fn main() {
+    let value = 3;
+    match value {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("other"),
+    }
+}
