d4c6978ee21dbc9d665a99c68910eeb59b6feb24
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 18:50:05 +0100

    feat: Add part 1 of FrontendMasters Rust course

---
+++ rust/rust/course-frontendmasters-the-rust-programming-language/part01/src/main.rs
@@ -0,0 +1,34 @@
+use core::f64;
+use std::u64;
+
+fn main() {
+    let city_name = "Rustville";
+
+    println!("The city of {}:\n", city_name);
+
+    print_population(1_324_578, 114_293, 108_097);
+}
+
+fn print_population(adults: u64, kids: u32, buildings: u32) {
+    // 👉 TODO compute population by adding adults to kids
+    //
+    // 💡 TIP: Use the `as` keyword to convert between numeric types!
+    let population = adults + (kids as u64);
+
+    // 👉 TODO compute buildings_per_person by dividing buildings by population
+    //
+    // 💡 TIP: To get a f64 answer here, both numerator and denominator must be f64 values
+    let buildings_per_person = (buildings as f64) / (population as f64);
+
+    println!("    Population: {}", population);
+    println!("        Adults: {}", adults);
+    println!("        Kids: {}", kids);
+    println!("    Buildings: {}", buildings);
+    println!("    Buildings per person: {}\n", buildings_per_person);
+
+    if buildings_per_person >= 1.0 {
+        println!("Everyone can have their own building!");
+    } else {
+        println!("Buildings must be shared!");
+    }
+}
