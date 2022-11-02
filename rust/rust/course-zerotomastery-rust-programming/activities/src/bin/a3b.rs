d3a13f1e610f67804cd68bb855d0e4e216b27223
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 20:17:05 +0100

    feat: complete activity 03b

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a3b.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a3b.rs
@@ -9,4 +9,13 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let value = 5;
+    if value > 5 {
+        println!(">5");
+    } else if value < 5 {
+        println!("<5");
+    } else {
+        printn!("=5");
+    }
+}
