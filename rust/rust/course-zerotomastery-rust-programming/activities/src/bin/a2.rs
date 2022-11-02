67a398263db76c71f3348d6d84dbeda2d4b5dbf1
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-02 19:37:08 +0100

    feat: complete activity 02

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a2.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a2.rs
@@ -8,4 +8,14 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+    display(add(1, 2))
+}
+
+fn display(n: i32) {
+    println!("{:?}", n)
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
