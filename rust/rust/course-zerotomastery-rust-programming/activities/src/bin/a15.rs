aae326700a1521f18205eabed8060ec5a72b7e07
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-03 20:52:02 +0100

    feat: complete activity 15

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a15.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a15.rs
@@ -11,4 +11,33 @@
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
 
-fn main() {}
+#[derive(Debug)]
+enum Ticket {
+    Backstage { name: String, price: f32 },
+    Standard(f32),
+    Vip { name: String, price: f32 },
+}
+
+fn main() {
+    let tickets = vec![
+        Ticket::Backstage {
+            name: "Bob".to_owned(),
+            price: 1000.0,
+        },
+        Ticket::Vip {
+            name: "Alice".to_owned(),
+            price: 0.0,
+        },
+        Ticket::Standard(120.50),
+    ];
+
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage { name, price } => {
+                println!("Backstage, name={:?}, price={:?}", name, price)
+            }
+            Ticket::Vip { name, price } => println!("Vip, name={:?}, price={:?}", name, price),
+            Ticket::Standard(price) => println!("Standard, price={:?}", price),
+        }
+    }
+}
