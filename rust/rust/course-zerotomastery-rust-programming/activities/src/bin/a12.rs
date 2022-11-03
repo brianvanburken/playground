df9fff1c0c665bdc0880279bfbf3d0c26b5d281a
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-03 19:26:06 +0100

    feat: complete activity 12

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a12.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a12.rs
@@ -10,4 +10,69 @@
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
 
-fn main() {}
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Red => println!("color=red"),
+            Color::Green => println!("color=green"),
+            Color::Blue => println!("color=blue"),
+        }
+    }
+}
+
+struct Dimensions {
+    width: f32,
+    height: f32,
+    depth: f32,
+}
+
+impl Dimensions {
+    fn new(width: f32, height: f32, depth: f32) -> Self {
+        Self {
+            width: width,
+            height: height,
+            depth: depth,
+        }
+    }
+
+    fn print(&self) {
+        println!(
+            "width={:?}, height={:?}, depth={:?}",
+            self.width, self.height, self.depth
+        )
+    }
+}
+
+struct ShippingBox {
+    weight: f32,
+    color: Color,
+    dimensions: Dimensions,
+}
+
+impl ShippingBox {
+    fn new(color: Color, weight: f32, dimensions: Dimensions) -> Self {
+        Self {
+            color: color,
+            weight: weight,
+            dimensions: dimensions,
+        }
+    }
+
+    fn print(&self) {
+        self.color.print();
+        self.dimensions.print();
+        println!("weight={:?}", self.weight)
+    }
+}
+
+fn main() {
+    let small_bx = Dimensions::new(1.0, 2.5, 1.0);
+    let bx = ShippingBox::new(Color::Red, 10.0, small_bx);
+    bx.print();
+}
