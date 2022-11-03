de11f1ee30635368a4b348a009a20393f398d27d
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-03 18:58:38 +0100

    feat: complete activity 11

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a11.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a11.rs
@@ -9,4 +9,21 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct GroceryItem {
+    id: i32,
+    quantity: i32,
+}
+
+fn main() {
+    let item = GroceryItem { id: 1, quantity: 3 };
+    display_id(&item);
+    display_quantity(&item)
+}
+
+fn display_id(item: &GroceryItem) {
+    println!("id: {:?}", item.id)
+}
+
+fn display_quantity(item: &GroceryItem) {
+    println!("quantity: {:?}", item.quantity)
+}
