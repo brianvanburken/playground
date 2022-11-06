a7822d89d7fa93f43c321fb36a682a074ee59f10
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-06 13:54:28 +0100

    feat: complete activity 18b

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a18b.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a18b.rs
@@ -22,4 +22,57 @@
 // * Print whether the employee may access the building
 //   * Must use a function that utilizes the question mark operator to do this
 
-fn main() {}
+enum Department {
+    MaintenanceCrew,
+    Marketing,
+    Manager,
+    LineSupervisor,
+    KitchenStaff,
+    AssemblyTechnician,
+}
+
+enum Employment {
+    Active,
+    Terminated,
+}
+
+struct Employee {
+    department: Department,
+    active_employment: Employment,
+}
+
+fn try_enter_building(employee: &Employee) -> Result<(), String> {
+    match employee {
+        Employee {
+            active_employment: Employment::Terminated,
+            ..
+        } => Err("Need to have active employment".to_owned()),
+        Employee {
+            department: Department::MaintenanceCrew,
+            ..
+        }
+        | Employee {
+            department: Department::Marketing,
+            ..
+        }
+        | Employee {
+            department: Department::Manager,
+            ..
+        } => Ok(()),
+        _ => Err("Employee does not have access to building".to_owned()),
+    }
+}
+
+fn print_access(employee: &Employee) -> Result<(), String> {
+    try_enter_building(employee)?;
+    println!("Access OK");
+    Ok(())
+}
+
+fn main() {
+    let bob = Employee {
+        department: Department::Manager,
+        active_employment: Employment::Terminated,
+    };
+    println!("{:?}", print_access(&bob));
+}
