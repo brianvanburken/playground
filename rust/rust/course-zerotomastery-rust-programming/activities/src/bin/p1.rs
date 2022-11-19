67691367fe3f089d406cf3b2769fe25975631582
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-11-19 11:04:12 +0100

    feat: complete stage 1 of Project 1

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/p1.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/p1.rs
@@ -28,5 +28,118 @@
 //   the functionality for that menu in isolation.
 // * A vector is the easiest way to store the bills at stage 1, but a
 //   hashmap will be easier to work with at stages 2 and 3.
+use std::io;
 
-fn main() {}
+#[derive(Debug, Clone)]
+pub struct Bill {
+    name: String,
+    amount: f64,
+}
+
+impl Bill {
+    fn new(name: String, amount: f64) -> Self {
+        Self { name, amount }
+    }
+}
+
+#[derive(Debug)]
+pub struct Bills {
+    ledger: Vec<Bill>,
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { ledger: vec![] }
+    }
+
+    fn add_bill(&mut self, bill: Bill) {
+        self.ledger.push(bill);
+    }
+}
+
+fn get_input_string() -> Option<String> {
+    let mut buffer = String::new();
+    while io::stdin().read_line(&mut buffer).is_err() {
+        println!("Please enter data again:")
+    }
+    let input = buffer.trim().to_owned();
+    if input == "" {
+        None
+    } else {
+        Some(input)
+    }
+}
+
+fn get_input_number() -> Option<f64> {
+    loop {
+        let input = match get_input_string() {
+            Some(input) => input,
+            None => return None,
+        };
+        let parsed_input: Result<f64, _> = input.parse();
+        match parsed_input {
+            Ok(amount) => return Some(amount),
+            Err(_) => println!("Please enter a correct number:"),
+        }
+    }
+}
+
+enum ManagerCommand {
+    AddBill,
+    ViewBills,
+}
+
+impl ManagerCommand {
+    fn from_str(input: &str) -> Option<Self> {
+        match input {
+            "1" => Some(Self::AddBill),
+            "2" => Some(Self::ViewBills),
+            _ => None,
+        }
+    }
+
+    fn print() {
+        println!("1. Add bill");
+        println!("2. View bills");
+    }
+}
+
+mod actions {
+    use crate::{get_input_number, get_input_string, Bill, Bills};
+
+    pub fn add_bill(bills: &mut Bills) {
+        println!("Input name of bill:");
+        let name = match get_input_string() {
+            Some(input) => input,
+            None => return,
+        };
+        println!("Amount:");
+        let amount = match get_input_number() {
+            Some(amount) => amount,
+            None => return,
+        };
+        let bill = Bill::new(name, amount);
+        bills.add_bill(bill);
+    }
+
+    pub fn view_bills(bills: &Bills) {
+        println!("{:?}", bills);
+    }
+}
+
+fn main() {
+    let mut bills = Bills::new();
+    loop {
+        println!("");
+        println!("~~~ Bills Manager Actions ~~~");
+        ManagerCommand::print();
+        println!("");
+        println!("Enter selection: ");
+        let input = get_input_string().expect("No user input");
+        match ManagerCommand::from_str(input.as_str()) {
+            Some(ManagerCommand::AddBill) => actions::add_bill(&mut bills),
+            Some(ManagerCommand::ViewBills) => actions::view_bills(&bills),
+            None => return,
+        }
+    }
+}
