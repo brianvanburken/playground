87d39bd97e077225154df365ae9e73a01f0c2d22
Brian van Burken <brianvanburken@users.noreply.github.com>
2022-12-18 13:26:26 +0100

    feat: complete activity 30

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a30.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a30.rs
@@ -16,6 +16,52 @@
 //   for the vehicle bodies/colors
 
 trait Body {}
+
 trait Color {}
 
-fn main() {}
+#[derive(Debug)]
+struct Vehicle<B, C>
+where
+    B: Body,
+    C: Color,
+{
+    body: B,
+    color: C,
+}
+
+impl<B, C> Vehicle<B, C>
+where
+    B: Body,
+    C: Color,
+{
+    pub fn new(body: B, color: C) -> Self {
+        Self { body, color }
+    }
+}
+
+#[derive(Debug)]
+struct Truck;
+impl Body for Truck {}
+
+#[derive(Debug)]
+struct Car;
+impl Body for Car {}
+
+#[derive(Debug)]
+struct Scooter;
+impl Body for Scooter {}
+
+#[derive(Debug)]
+struct Gray;
+impl Color for Gray {}
+
+#[derive(Debug)]
+struct Black;
+impl Color for Black {}
+
+fn main() {
+    let black_car = Vehicle::new(Car, Black);
+    let gray_scooter = Vehicle::new(Scooter, Gray);
+    println!("Vehicle 1 {:?}", black_car);
+    println!("Vehicle 2 {:?}", gray_scooter);
+}
