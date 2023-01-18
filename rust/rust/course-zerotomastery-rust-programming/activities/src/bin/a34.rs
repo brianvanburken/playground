35c4fa1e3fd8ef7108c857e6c012fec376f5d7e0
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-01-18 20:44:50 +0100

    feat: complete activity 34

--- rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a34.rs
+++ rust/rust/course-zerotomastery-rust-programming/activities/src/bin/a34.rs
@@ -16,4 +16,65 @@
 // Notes:
 // * Optionally use generics for each state
 
-fn main() {}
+#[derive(Debug)]
+struct Luggage<State> {
+    id: usize,
+    state: State,
+}
+
+#[derive(Debug)]
+struct CheckedIn;
+#[derive(Debug)]
+struct OnLoading;
+#[derive(Debug)]
+struct OffLoading;
+#[derive(Debug)]
+struct AwaitingPickup;
+#[derive(Debug)]
+struct EndCustody;
+
+impl Luggage<CheckedIn> {
+    fn new(id: usize) -> Self {
+        Self {
+            id,
+            state: CheckedIn,
+        }
+    }
+
+    fn load_on_plane(self) -> Luggage<OnLoading> {
+        self.transition(OnLoading)
+    }
+}
+
+impl Luggage<OnLoading> {
+    fn load_off_plane(self) -> Luggage<OffLoading> {
+        self.transition(OffLoading)
+    }
+}
+
+impl Luggage<OffLoading> {
+    fn put_on_conveyer(self) -> Luggage<AwaitingPickup> {
+        self.transition(AwaitingPickup)
+    }
+}
+
+impl Luggage<AwaitingPickup> {
+    fn picked_up(self) -> Luggage<EndCustody> {
+        self.transition(EndCustody)
+    }
+}
+
+impl<State> Luggage<State> {
+    fn transition<NextState>(self, state: NextState) -> Luggage<NextState> {
+        Luggage { id: self.id, state }
+    }
+}
+
+fn main() {
+    let luggage = Luggage::new(1)
+        .load_on_plane()
+        .load_off_plane()
+        .put_on_conveyer()
+        .picked_up();
+    dbg!(luggage);
+}
