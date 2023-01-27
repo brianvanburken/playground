7ed0fe519b8ddaaa2a11cc00ac41f21c3e8e63c9
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-01-27 21:00:09 +0100

    feat: finish chapter 2

---
+++ rust/rust/book-rust-web-development/src/main.rs
@@ -0,0 +1,47 @@
+use std::str::FromStr;
+use std::io::{Error, ErrorKind};
+
+use warp::Filter;
+
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::get()
+        .map(|| format!("Hello, World!"));
+
+    warp::serve(hello)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
