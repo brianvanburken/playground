df5987d98cdd6d7a15c6a01b18701d575e92f91b
Brian van Burken <brianvanburken@users.noreply.github.com>
2024-12-15 18:53:12 +0100

    feat(testing-elixir): update deps to work with elixir 1.17

--- elixir/elixir/book-testing-elixir/chapter01/soggy_waffle/mix.exs
+++ elixir/elixir/book-testing-elixir/chapter01/soggy_waffle/mix.exs
@@ -1,11 +1,11 @@
-#---
+# ---
 # Excerpted from "Testing Elixir",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
 # We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/lmelixir for more book information.
-#---
+# ---
 defmodule SoggyWaffle.MixProject do
   use Mix.Project
 
@@ -13,7 +13,7 @@ defmodule SoggyWaffle.MixProject do
     [
       app: :soggy_waffle,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
