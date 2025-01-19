c185eb05c11c59434f663dd5d106497038e1d6f2
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-01-19 14:58:51 +0100

    feat(ash-framework): store previous name in model on artist name change

--- elixir/ash/book-ash-framework/lib/tunez/music/artist.ex
+++ elixir/ash/book-ash-framework/lib/tunez/music/artist.ex
@@ -7,8 +7,28 @@ defmodule Tunez.Music.Artist do
   end
 
   actions do
-    defaults [:read, :destroy, :create, :update]
+    defaults [:read, :destroy, :create]
     default_accept [:name, :biography]
+
+    update :update do
+      require_atomic? false
+
+      accept [:name, :biography]
+
+      change fn changeset, _context ->
+               new_name = Ash.Changeset.get_attribute(changeset, :name)
+               previous_name = Ash.Changeset.get_data(changeset, :name)
+               previous_names = Ash.Changeset.get_data(changeset, :previous_names)
+
+               names =
+                 [previous_name | previous_names]
+                 |> Enum.uniq()
+                 |> Enum.reject(&(&1 == new_name))
+
+               Ash.Changeset.change_attribute(changeset, :previous_names, names)
+             end,
+             where: [changing(:name)]
+    end
   end
 
   changes do
