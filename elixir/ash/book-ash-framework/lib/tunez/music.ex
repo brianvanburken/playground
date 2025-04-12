c0eb3b36e55247d5c424e5db32182da9ec05e50d
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-04-12 20:51:05 +0200

    feat(ash-book): use db to list artists

--- elixir/ash/book-ash-framework/lib/tunez/music.ex
+++ elixir/ash/book-ash-framework/lib/tunez/music.ex
@@ -5,7 +5,7 @@ defmodule Tunez.Music do
   resources do
     resource Tunez.Music.Artist do
       define :create_artist, action: :create
-      define :read_artist, action: :read
+      define :read_artists, action: :read
       define :get_artist_by_id, action: :read, get_by: :id
       define :update_artist, action: :update
       define :destroy_artist, action: :destroy
