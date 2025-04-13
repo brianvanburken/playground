e382105c65114bfe937b41bbb48cb453ccb75f2a
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-04-13 12:15:45 +0200

    feat(ash-book): setup create and update artist

--- elixir/ash/book-ash-framework/lib/tunez/music.ex
+++ elixir/ash/book-ash-framework/lib/tunez/music.ex
@@ -1,6 +1,7 @@
 defmodule Tunez.Music do
   use Ash.Domain,
-    otp_app: :tunez
+    otp_app: :tunez,
+    extensions: [AshPhoenix]
 
   resources do
     resource Tunez.Music.Artist do
--- elixir/ash/book-ash-framework/lib/tunez_web/live/artists/form_live.ex
+++ elixir/ash/book-ash-framework/lib/tunez_web/live/artists/form_live.ex
@@ -1,8 +1,20 @@
 defmodule TunezWeb.Artists.FormLive do
   use TunezWeb, :live_view
 
+  def mount(%{"id" => artist_id}, _session, socket) do
+    artist = Tunez.Music.get_artist_by_id!(artist_id)
+    form = Tunez.Music.form_to_update_artist(artist)
+
+    socket =
+      socket
+      |> assign(:form, to_form(form))
+      |> assign(:page_title, "Update Artist")
+
+    {:ok, socket}
+  end
+
   def mount(_params, _session, socket) do
-    form = %{}
+    form = Tunez.Music.form_to_create_artist()
 
     socket =
       socket
@@ -35,11 +47,34 @@ defmodule TunezWeb.Artists.FormLive do
     """
   end
 
-  def handle_event("validate", %{"form" => _form_data}, socket) do
+  def handle_event("validate", %{"form" => form_data}, socket) do
+    socket =
+      update(socket, :form, fn form ->
+        AshPhoenix.Form.validate(form, form_data)
+      end)
+
     {:noreply, socket}
   end
 
-  def handle_event("save", %{"form" => _form_data}, socket) do
+  def handle_event("save", %{"form" => form_data}, socket) do
+    case AshPhoenix.Form.submit(socket.assigns.form, params: form_data) do
+      {:ok, artist} ->
+        socket =
+          socket
+          |> put_flash(:info, "Artist saved successfully")
+          |> push_navigate(to: ~p"/artists/#{artist}")
+
+        {:noreply, socket}
+
+      {:error, form} ->
+        socket =
+          socket
+          |> put_flash(:error, "Could not save artist data")
+          |> assign(:form, form)
+
+        {:noreply, socket}
+    end
+
     {:noreply, socket}
   end
 end
