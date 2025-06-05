a33f5b3f4f2bf3dd70999337a445c017587681c7
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-06-05 13:53:35 +0200

    feat: redirect / to /guess

--- elixir/phoenix/book-programming-phoenix-liveview/pento/lib/pento_web/controllers/page_controller.ex
+++ elixir/phoenix/book-programming-phoenix-liveview/pento/lib/pento_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule PentoWeb.PageController do
   use PentoWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: ~p"/guess")
   end
 end
