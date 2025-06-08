ed1ed4f23be4ec91668be37912e0f38636b14976
Brian van Burken <brianvanburken@users.noreply.github.com>
2025-06-08 11:48:49 +0200

    feat(book-liveview): wip chapter 3

--- elixir/phoenix/book-programming-phoenix-liveview/pento/lib/pento_web/live/product_live/index.ex
+++ elixir/phoenix/book-programming-phoenix-liveview/pento/lib/pento_web/live/product_live/index.ex
@@ -6,7 +6,10 @@ defmodule PentoWeb.ProductLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, stream(socket, :products, Catalog.list_products())}
+    {:ok,
+     socket
+     |> assign(:greeting, "Welcome to Pento!")
+     |> stream(:products, Catalog.list_products())}
   end
 
   @impl true
--- elixir/phoenix/book-programming-phoenix-liveview/pento/lib/pento_web/live/product_live/show.ex
+++ elixir/phoenix/book-programming-phoenix-liveview/pento/lib/pento_web/live/product_live/show.ex
@@ -19,3 +19,13 @@ defmodule PentoWeb.ProductLive.Show do
   defp page_title(:show), do: "Show Product"
   defp page_title(:edit), do: "Edit Product"
 end
+
+# Questions chapter 3
+# - Which route gets invoked when you click the link on the Index page to view a given product?
+#   A: this file
+# - What data does ProductLive.Show.mount/3 add to the socket?
+#   A: Nothing
+# - How does the ProductLive.Show live view use the handle_params/3 callback?
+#   A: to set the product using the ID from the params and the title based on the action
+# - How does the ProductLive.Show template render the Product Edit form and what events does that form support?
+#   A: It renders the details of a product and a button on top to edit through a modal containing a live form
